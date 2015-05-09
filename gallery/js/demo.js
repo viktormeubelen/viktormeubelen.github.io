2e61f2dd8d5f2c8634419349430b643a73d8bb81
Fedor Biryukov <fedor.birjukov@gmail.com>
2015-05-09 17:43:56 +0200

    My home page

---
+++ gallery/js/demo.js
@@ -0,0 +1,62 @@
+/*
+ * Bootstrap Image Gallery JS Demo 3.0.1
+ * https://github.com/blueimp/Bootstrap-Image-Gallery
+ *
+ * Copyright 2013, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/*jslint unparam: true */
+/*global blueimp, $ */
+
+$(function () {
+    'use strict';
+
+    // Load demo images from flickr:
+    $.ajax({
+        url: 'https://api.flickr.com/services/rest/',
+        data: {
+            format: 'json',
+            method: 'flickr.people.getPublicPhotos',
+            api_key: '94bd93219d4bd4d6bf773a03d8a1cc21',
+            user_id: '133078670@N04'
+        },
+        dataType: 'jsonp',
+        jsonp: 'jsoncallback'
+    }).done(function (result) {
+        var linksContainer = $('#links'),
+            baseUrl;
+        // Add the demo images as links with thumbnails to the page:
+        $.each(result.photos.photo, function (index, photo) {
+            baseUrl = 'http://farm' + photo.farm + '.static.flickr.com/' +
+                photo.server + '/' + photo.id + '_' + photo.secret;
+            $('<a/>')
+                .append($('<img>').prop('src', baseUrl + '_s.jpg'))
+                .prop('href', baseUrl + '_b.jpg')
+                .prop('title', photo.title)
+                .attr('data-gallery', '')
+                .appendTo(linksContainer);
+        });
+    });
+
+    $('#borderless-checkbox').on('change', function () {
+        var borderless = $(this).is(':checked');
+        $('#blueimp-gallery').data('useBootstrapModal', !borderless);
+        $('#blueimp-gallery').toggleClass('blueimp-gallery-controls', borderless);
+    });
+    
+    $('#borderless-checkbox').click();
+
+    $('#fullscreen-checkbox').on('change', function () {
+        $('#blueimp-gallery').data('fullScreen', $(this).is(':checked'));
+    });
+
+    $('#image-gallery-button').on('click', function (event) {
+        event.preventDefault();
+        blueimp.Gallery($('#links a'), $('#blueimp-gallery').data());
+    });
+
+});
